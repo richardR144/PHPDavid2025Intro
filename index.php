d93655ff5fa6d8d19203c8b714c15fbe58fa9cc5
richardR144 <richard.rigollet@lapiscine.pro>
2025-03-10 14:29:25 +0100

    exo âge

--- index.php
+++ index.php
@@ -1,15 +1,31 @@
 <?php  
 //création d'une variable en PHP
 $name="David";
+$bestFood="tagine";
+$age=30
 ?>
 
 <!--Je demande au php de généré du html avec un h1, incluant bonjour suivi de la valeur de la variable
  $name suivi de la fermeture de la balise h1. Pour faire une instruction php je réouvre la balise echo 
  en php d'afficher sa valeur, qui s' affiche dans le navigateur-->
 
-<h1>Hello <?php echo $name; ?></h1>
 
 
+<h1>Hello <?php echo $name;?> et j'aime le <?php echo $bestFood;?> . Mon âge : <?php echo $age?> ans</h1>
+
+<p>
+    <!--Je demande avec mon if si je suis majeur et sinon je suis mineur
+    <?php if($age >= 18)
+    {?>
+        <p>Vous êtes majeur</p>
+    <?php 
+    }
+    else
+    {?>
+        <p>Vous êtes mineur</p>
+    <?php 
+}?>
+</p>
 
 
 
