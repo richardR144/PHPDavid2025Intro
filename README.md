74b3f6ab50a968fe1cab8db16f6779765078851c
richardR144 <richard.rigollet@lapiscine.pro>
2025-03-10 14:08:37 +0100

    intro php

---
+++ README.md
@@ -0,0 +1,1 @@
+# PHPDavid2025Intro
---
+++ index.php
@@ -0,0 +1,31 @@
+<?php  
+//création d'une variable en PHP
+$name="David";
+?>
+
+<!--Je demande au php de généré du html avec un h1, incluant bonjour suivi de la valeur de la variable
+ $name suivi de la fermeture de la balise h1. Pour faire une instruction php je réouvre la balise echo 
+ en php d'afficher sa valeur, qui s' affiche dans le navigateur-->
+
+<h1>Hello <?php echo $name; ?></h1>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
